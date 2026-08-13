1f3fdd2554b9d6b8af4783974fb68d5ea043738a
Madhuvanthi C E <madhuvanthi.2747@gmail.com>
2026-08-13 19:32:41 +0530

    Create script.js

---
+++ script.js
@@ -0,0 +1,11 @@
+<script>
+function openResume(event) {
+  event.preventDefault();
+
+  document.getElementById("resumeModal").style.display = "flex";
+}
+
+function closeResume() {
+  document.getElementById("resumeModal").style.display = "none";
+}
+</script>
