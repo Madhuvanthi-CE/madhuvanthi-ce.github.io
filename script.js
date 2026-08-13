312351d645ec25cc9ddbcee0492bb8af9c69a91d
Madhuvanthi C E <madhuvanthi.2747@gmail.com>
2026-08-13 19:36:51 +0530

    Refactor mobile menu and resume modal functionality
    Refactor mobile menu toggle and resume modal functions.

--- script.js
+++ script.js
@@ -1,11 +1,22 @@
 <script>
-function openResume(event) {
-  event.preventDefault();
+  // Mobile Menu
+  const toggle = document.getElementById("menu-toggle");
+  const nav = document.getElementById("nav-links");
 
-  document.getElementById("resumeModal").style.display = "flex";
-}
+  toggle.addEventListener("click", () => {
+    nav.classList.toggle("active");
+    toggle.classList.toggle("open");
+  });
 
-function closeResume() {
-  document.getElementById("resumeModal").style.display = "none";
-}
+
+  // Resume Popup
+  function openResume(event) {
+    event.preventDefault();
+
+    document.getElementById("resumeModal").style.display = "flex";
+  }
+
+  function closeResume() {
+    document.getElementById("resumeModal").style.display = "none";
+  }
 </script>
